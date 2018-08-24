a324c56d9d3a076c2f62069938edff1f3a0572c5
JuAlexandre <julien.alexandre26@gmail.com>
2018-08-24 12:13:52 +0200

    updating gitignore file

--- upload.php
+++ upload.php
@@ -1,19 +1,12 @@
 <?php
 
-//print_r($_FILES['image']);
-
-// if 'upload image' button is clicked
 if (isset($_FILES['image'])) {
-    // Get image name
     $image = $_FILES['image']['name'];
-
-    // image file directory
     $target = "images/" . basename($image);
-
     if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
         echo "http://localhost:8000/" . $target;
         exit();
-    }else{
+    } else{
         echo "Failed to upload image";
         exit();
     }
